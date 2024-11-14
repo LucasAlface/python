c53122bb195d901d0a2208977181da787f6329c1
Lucas Soares <147398076+LucasAlface@users.noreply.github.com>
2024-11-14 12:38:04 +0000

    Add files via upload

---
+++ Python_2024/projetos/Jogo_Nave/dicionarios.py
@@ -0,0 +1,14 @@
+# Este ficheiro serve para ter um dicionário com as constantes das cores do colorama para poderem ser usados em variáveis
+
+from colorama import Fore
+# Dicionário
+Cores = {
+    "RED": Fore.RED,
+    "GREEN": Fore.GREEN,
+    "YELLOW": Fore.YELLOW,
+    "BLUE": Fore.BLUE,
+    "CYAN": Fore.CYAN,
+    "MAGENTA": Fore.MAGENTA,
+    "WHITE": Fore.WHITE,
+    "BLACK": Fore.BLACK
+}
